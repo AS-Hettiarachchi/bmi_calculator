770156d800aabb1360bc908992ddf1945ce12b86
AS-Hettiarachchi <149992549+AS-Hettiarachchi@users.noreply.github.com>
2024-02-19 10:47:09 +0530

    layout fix and add button

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: avoid_print
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -19,7 +22,84 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text("BMI Calculator"),
+        backgroundColor: Colors.lime[400],
+      ),
+      body: Column(
+        children: [
+          const Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(
+                    left: 200.0, right: 200.0, top: 50.0, bottom: 20.0),
+                child: Text("image pos"),
+              )
+            ],
+          ),
+          const Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.only(
+                      left: 50.0, right: 50.0, top: 100.0, bottom: 10.0),
+                  child: Text("Weight"),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.only(
+                      left: 50.0, right: 50.0, top: 100.0, bottom: 10.0),
+                  child: Text("Age"),
+                ),
+              ),
+            ],
+          ),
+          const Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.only(
+                      left: 50.0, right: 50.0, top: 10.0, bottom: 50.0),
+                  child: TextField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      labelText: 'Enter your weight in KG',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.only(
+                      left: 50.0, right: 50.0, top: 10.0, bottom: 50.0),
+                  child: TextField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      labelText: 'Enter your age in years',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.only(left: 200.0, right: 200.0),
+                  child:
+                      ElevatedButton(onPressed: test, child: Text("Calculate")),
+                ),
+              ),
+            ],
+          )
+        ],
       ),
     );
+  }
+
+  test() {
+    print("aa");
   }
 }
